37fcea7584b08207bede9952bf0f56d25a10f261
Yarin Yossefia <yarin5563@gmail.com>
2025-06-20 20:47:14 +0300

    remove service account permissions

--- terraform/tenants/company-a/production/main.tf
+++ terraform/tenants/company-a/production/main.tf
@@ -1,1 +1,7 @@
- 
+module "gke" {
+  source       = "../../../modules/gke"
+  project_id   = var.project_id
+  cluster_name = var.cluster_name
+  location     = var.region
+  machine_type = var.machine_type
+} 
--- terraform/tenants/company-a/production/terraform.tfvars
+++ terraform/tenants/company-a/production/terraform.tfvars
@@ -1,1 +1,6 @@
+project_id   = "dataloop-project-463316"
+region       = "us-central1"
+zone         = "us-central1-a"
+cluster_name = "company-a-production-gke-cluster"
+machine_type = "e2-medium"
  
